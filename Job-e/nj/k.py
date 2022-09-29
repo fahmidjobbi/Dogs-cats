909fbbda8c3e68a529f69774e5c5354eb35ee43c
Fahmi DJOBBI <djobbioofahmi@gmail.com>
2022-09-29 11:28:11 +0100

    fff

---
+++ Job-e/nj/k.py
@@ -0,0 +1,16 @@
+from re import T
+import tkinter as tk
+from PIL import Image,ImageTk
+from email.message import Message
+from timeit import repeat
+import tkinter
+import tkinter.messagebox
+import customtkinter
+from tkinter import Tk, Frame, Menu, ttk,Toplevel
+from PIL import Image,ImageTk
+import os
+import tkinter.filedialog as filedialog
+import numpy as np
+import joblib as joblib
+import pandas as pd 
+from keras.models import load_model
